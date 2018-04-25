621c954bc45e337dbbb7b46b23c62016bd0035a2
nwelchr <nwelchr@gmail.com>
2018-04-25 09:30:50 -0700

    Merge

--- vr/client.js
+++ vr/client.js
@@ -5,29 +5,40 @@
 // Auto-generated content.
 import {VRInstance} from 'react-vr-web';
 
+const GearVRRaycaster = {
+  getType: () => "Gear VR",
+  getRayOrigin: () => [0, 0, 0],
+  getRayDirection: () => [0, 0, -1],
+  drawsCursor: () => true
+};
+
 import DocumentGet from './documentGet.js'; 
 
 function init(bundle, parent, options) {
-
+  let vr;
+  let { mobile } = options;
   const browserInfo = new DocumentGet(); 
 
-  
-  const vr = new VRInstance(bundle, 'WelcomeToVR', parent, {
-    
-    nativeModules: [browserInfo],
-    ...options,
-  });
+  vr = mobile
+    ? new VRInstance(bundle, "WelcomeToVR", parent, {
+        nativeModules: [browserInfo], 
+        raycasters: [GearVRRaycaster],
+        cursorVisibility: "visible",
+        ...options
+      })
+    : new VRInstance(bundle, "WelcomeToVR", parent, {
+        nativeModules: [browserInfo],    
+        ...options
+      });
 
   browserInfo._setRNContext(vr.rootView.context);
 
   vr.render = function() {
-    // console.log(window.document);
     // Any custom behavior you want to perform on each frame goes here
   };
   // Begin the animation loop
-  
   vr.start();
   return vr;
 }
 
-window.ReactVR = {init};
+window.ReactVR = { init };
