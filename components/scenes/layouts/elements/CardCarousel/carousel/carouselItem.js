2cb9228aa34eda643867dcd43bb08d64940b3b98
Drew Stukey <stukey524@gmail.com>
2018-04-25 21:25:13 -0700

    Modify card carousel to include text

--- components/scenes/layouts/elements/CardCarousel/carousel/carouselItem.js
+++ components/scenes/layouts/elements/CardCarousel/carousel/carouselItem.js
@@ -2,37 +2,92 @@ import React from 'react';
 import {
   Image,
   View,
+  Text,
 } from 'react-vr';
+
+import { IMAGE, TEXT } from './cardTypes.js';
+import TextCard from '../../TextCard.js';
 
 class CarouselItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       opacity: 1,
-      currentImage: this.props.card,
+      currentItem: this.props.card,
+      textSlices: [],
+      currentTextSlice: -1,
     }
+    this.textSet = this.textSet.bind(this);
   }
 
   componentWillReceiveProps(next) {
     if(this.props.card != next.card){
-      this.setState({currentImage: next.card});
+      this.setState({currentItem: next.card});
     }
+  }
+
+  componentDidMount() {
+    this.textSet();
+  }
+
+  textSet(iterateCount = 200) {
+
+      const text = this.props.children || this.props.text;
+
+      if (text.length < 200) {
+        this.setState({currentTextSlice: 0});
+      } else {
+          for(let i = 0; i < text.length; i += iterateCount){
+              if (text.length - i >= iterateCount){
+                  this.state.textSlices.push(text.slice(i, (i + iterateCount)));
+              }else{
+                  this.state.textSlices.push(text.slice(i, (i + iterateCount)));
+              }
+          }
+          this.setState({currentTextSlice: 0});
+      }
   }
 
   render() {
 
+    const imageType = this.props.cardType === IMAGE;
+    const textType = this.props.cardType === TEXT;
+
     return (
       <View>
-        <Image
-          style={{
-            margin: 5,
-            width: '100%',
-            height: '100%',
+        { imageType ?
+            (<Image
+              style={{
+                margin: 5,
+                width: '100%',
+                height: '100%',
 
-          }}
-            source={this.state.currentImage}
-
-        />
+              }}
+              source={this.state.currentItem}
+            />
+          ) : textType ? (
+            <View style={{height:500, width: 500 }}>
+            <Text
+                style={{
+                    flex: 1,
+                    backgroundColor: 'rgba(0,200,200,0.5)',
+                    width: "100%",
+                    height: "100",
+                    maxWidth: 400,
+                    maxHeight: 500,
+                    lineHeight: 80,
+                    fontSize: 40,
+                    fontWeight: '400',
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    margin: 2,
+                }}
+              >{this.state.textSlices[this.state.currentTextSlice]}</Text>
+            </View>
+            ) : (
+              <Text>Stuff</Text>
+            )
+          }
       </ View>
     )
   }
--- index.vr.js
+++ index.vr.js
@@ -1,71 +1,61 @@
 import React from 'react';
 import {
   AppRegistry,
+  NativeModules,
   asset,
+  VrButton,
   Pano,
   Text,
   View,
 } from 'react-vr';
+import Dashboard from './components/scenes/Dashboard.js';
 
-import App from './NavBarWConditionalRendering/app';
-import Gallery1 from './NavBarWConditionalRendering/gallery/gallery1';
-import Gallery2 from './NavBarWConditionalRendering/gallery/gallery2';
-import Gallery3 from './NavBarWConditionalRendering/gallery/gallery3';
-import BottomNavBar from './NavBarWConditionalRendering/nav_bar/bottom_nav_bar';
+const theDocs = NativeModules.DocumentGet;
 
-import { backgroundImage } from './helperFiles/styleSheet.js';
+
 
 export default class WelcomeToVR extends React.Component {
-  constructor() {
+  constructor(){
     super();
     this.state = {
-      gallery: <Gallery1/>
-    }
-
-    this.changeGallery = this.changeGallery.bind(this);
+     heaven: 'hello'
+    };
   }
 
-  changeGallery(galleryId) {
-    let gallery;
-
-    // TODO: extract from navbarContent in content.js
-    switch(galleryId) {
-      case 'gallery1':
-        gallery = <Gallery1/>;
-        break;
-      case 'gallery2':
-        gallery = <Gallery2/>;
-        break;
-      case 'gallery3':
-        gallery = <Gallery3/>;
-        break;
-    }
-
-    this.setState({gallery});
+  componentWillMount(){
+  
+    console.log('WTF');
+    console.log('checking state', this.state.heaven); 
+    theDocs.getDocument(result => {
+      this.setState({
+        heaven: result
+      });
+    });
   }
 
+  testMethod() {
+   
+    //this sets the state to have whatever we pass in from the DOM 
+   
+
+
+
+  }
   render() {
-    // TODO: put in content.js
-    content = [
-      {label: 'Home', link: 'gallery1'},
-      {label: 'About', link: 'gallery2'},
-      {label: 'Gallery', link: 'gallery3'}
-    ];
+    
 
+    let solution = this.state.heaven; 
+    console.log(solution);
     return (
       <View>
-        <Pano source={{uri: backgroundImage}}/>
-        <App>
-        {this.state.gallery}
-        </App>
-        <BottomNavBar 
-          content={content}
-          changeGallery={this.changeGallery.bind(this)}
-          gallery={this.state.gallery}
-          />
+        <Pano source={asset('space.jpg')}/>
+        <VrButton onClick={() => this.testMethod()}>
+        </VrButton>
+        <Dashboard/> 
       </View>
     );
   }
-};
+
+}
 
 AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
