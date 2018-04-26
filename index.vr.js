e3f921954bed7830b2d6d33e5a40bb3aa336354b
JonHalloran <angmar139@hotmail.com>
2018-04-25 18:46:44 -0700

    Clean index.vf

--- index.vr.js
+++ index.vr.js
@@ -23,9 +23,6 @@ export default class WelcomeToVR extends React.Component {
   }
 
   componentWillMount(){
-  
-    console.log('WTF');
-    console.log('checking state', this.state.store); 
     theDocs.getDocument(result => {
       this.setState({
         store: result
@@ -33,45 +30,11 @@ export default class WelcomeToVR extends React.Component {
     });
   }
 
-  testMethod() {
-   
-    //this sets the state to have whatever we pass in from the DOM 
-   
-
-
-
-  }
+  testMethod() {}
   render() {
-    console.log("store", this.state.store);
-    let toRender = this.state.store.map((el)=>{
-      console.log("el", el);
-      switch(el.type){
-        case "text":
-          console.log("in text", console.log(el));
-          return (<Text
-                    style={{
-                        backgroundColor: 'rgba(0,200,200,0.5)',
-                        color: '#FFF',
-                        fontSize: .2,
-                        textAlign: 'center',
-                        textAlignVertical: 'center',
-                        //combo of height && max height to ensure one component doesn't take up the whole 180º
-                        height: 1,
-                        maxHeight: 1,
-                        width: 1,
-                        maxWidth: 1,
-                        layoutOrigin: [0.5, 0.5],
-                        transform: [{translate: [0, 0, -2]}]
-                    }}>
-                    {`${el.content}`}  
-                  </Text> );
-      }
-    });
-    console.log("toRender", toRender);
     return (
       <View>
         <Pano source={asset('space.jpg')}/>
-        {toRender}
           <VrButton onClick={() => this.testMethod()}>
           </VrButton>
         <Dashboard content={this.state.store}/> 
