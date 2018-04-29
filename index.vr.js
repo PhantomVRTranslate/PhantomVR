7bd736b695b67ff0b752b2b9b80e125637520b66
nwelchr <nwelchr@gmail.com>
2018-04-29 14:09:58 -0700

    Change around index for testing

--- index.vr.js
+++ index.vr.js
@@ -9,98 +9,49 @@ import {
   View
 } from "react-vr";
 
-import { merge } from "lodash";
-
-import BatchedBridge from "react-native/Libraries/BatchedBridge/BatchedBridge";
-import BrowserBridge from "./vr/BrowserBridge.js";
-import PageConstructor from "./components/PageConstructor.js";
-
-const browserBridge = new BrowserBridge();
-BatchedBridge.registerCallableModule(BrowserBridge.name, browserBridge);
-
-const theDocs = NativeModules.DocumentGet;
-
 import App from "./components/app";
 import Title from "./components/title";
 import ContentPlane from "./components/ContentPlane.js";
 import { backgroundImage } from "./helperFiles/styleSheet.js";
-import NavBar from "./components/navbar/Navbar.js";
+import Navbar from "./components/navbar/Navbar.js";
 
 export default class WelcomeToVR extends React.Component {
   constructor() {
     super();
+    
     this.state = {
       enterScene: false,
-      store: {},
-      clickEvent: this.clickEvent.bind(this)
     };
-    this.mergeState = this.mergeState.bind(this);
+
     this.activateScene = this.activateScene.bind(this);
-  }
-
-  clickEvent(classname, i) {
-    theDocs.triggerEvent(classname, i);
-  }
-
-  mergeState(addContent, removeContent) {
-      console.log("merge state", addContent, removeContent);
-    let store = merge({}, this.state.store, addContent);
-    removeContent.forEach(content => {
-      delete store[content];
-    });
-
-    this.setState({
-      store: store
-    });
-  }
-
-  componentWillMount() {
-    this.unsubscribe = browserBridge.subscribe(this.mergeState);
-    this.setState({ enterScene: false });
-    theDocs.getDocument(result => {
-      let mergedStore = this.state.store + result;
-    });
-  }
-
-  makeNavLinks() {
-    let theContent = Object.values(this.state.store) || [];
-    let navLinks = [];
-    // debugger;
-    theContent.forEach(content => {
-      if (content.type === "navlink-vr") {
-        navLinks.push({ label: content.navTitle, link: content.key });
-      }
-    });
-    return navLinks;
   }
 
   activateScene() {
     this.setState({ enterScene: true });
   }
 
-  render() { 
-    let navbarContent = this.makeNavLinks();
+  render() {
     return (
       <View>
-< Pano source = {{uri: "https://rawgit.com/PhantomVRTranslate/PhantomVR/master/static_assets/space.jpg"}}/>
-        <Title activateScene={this.activateScene} />
-        {this.state.enterScene ? (
-          <ContentPlane>
-            <PageConstructor
-              store={this.state.store}
-              clickEvent={this.state.clickEvent}
-            />
-          </ContentPlane>
-        ) : (
-          <View />
-        )}
-        <NavBar
-          content={navbarContent}
-          changePage={this.state.clickEvent}
-          page={{ type: { name: "hello" } }}
-        />
+        {/* <Pano source={{uri: 'https://c1.staticflickr.com/1/819/26673769157_b83ac4a9f6_o.jpg'}}/> // forest */}
+        {/* <Pano source={{uri: 'https://c1.staticflickr.com/1/818/27568453618_37132d75e8_o.jpg'}}/> // beach */}
+        {/* <Pano source={{uri: 'https://c1.staticflickr.com/1/577/20214930404_d30c781c47_o.jpg'}}/> // fractal */}
+        {/* <Pano source={{uri: 'https://c2.staticflickr.com/8/7720/17491232401_4fee3698ff_o.jpg'}}/> // snow */}
+        {/* <Pano source={{uri: 'https://c1.staticflickr.com/1/192/500978642_93d4446af7_o.jpg'}}/> // inari */}
+        <Title
+          title='PhantomVR' 
+          activateScene={this.activateScene}
+          titleStyling={{
+            color: 'orange'
+          }}
+          startText='Welcome'
+          startTextStyling={{
+            color: 'blue'
+          }} />
+        { this.state.enterScene ? <App /> : <View /> }
       </View>
     );
+
   }
 }
 
